3df917e9530cf0c19acb674202664cabc4bc53d8
Agustín González <agonzalez@msa.com.ar>
2024-05-17 12:53:10 -0300

    Add support for multi-byte glyphs

--- src/main.c
+++ src/main.c
@@ -291,6 +291,39 @@ void face_get_string_extent (const FT_Face face, const UTF32 *s,
   *y = y_extent;
   }
 
+  /*===========================================================================
+
+  next_utf8_glyph_length
+
+  Gets the length of next glyph in a UTF-8 sequence.
+
+  Returns -1 if the next glyph is not UTF-8.
+
+  =========================================================================*/
+int next_utf8_glyph_length(const UTF8 *word) {
+    assert(word != NULL);
+
+    // 1-byte glyph (0xxxxxxx).
+    if ((*word & 0x80) == 0) {
+        return 1;
+    }
+    // 2-byte glyph (110xxxxx).
+    else if ((*word & 0xE0) == 0xC0) {
+        return 2;
+    }
+    // 3-byte glyph (1110xxxx).
+    else if ((*word & 0xF0) == 0xE0) {
+        return 3;
+    }
+    // 4-byte glyph (11110xxx).
+    else if ((*word & 0xF8) == 0xF0) {
+        return 4;
+    }
+
+    // Invalid UTF-8 glyph.
+    return -1;
+}
+
 /*===========================================================================
 
   utf8_to_utf32 
@@ -304,18 +337,57 @@ void face_get_string_extent (const FT_Face face, const UTF32 *s,
     to 32-bit.
 
   =========================================================================*/
-UTF32 *utf8_to_utf32 (const UTF8 *word)
-  {
-  assert (word != NULL);
-  int l = strlen ((char *)word);
-  UTF32 *ret = malloc ((l + 1) * sizeof (UTF32));
-  for (int i = 0; i < l; i++)
-    {
-    ret[i] = (UTF32) word[i];
+UTF32 *utf8_to_utf32(const UTF8 *utf8_word)
+{
+    assert(utf8_word != NULL);
+
+    // Compute the length of the resulting UTF-32 sequence.
+    int word_length = 0;
+    const UTF8 *utf8_word_ptr = utf8_word;
+
+    while (*utf8_word_ptr) {
+        int curr_glyph_length = next_utf8_glyph_length(utf8_word_ptr);
+        utf8_word_ptr += curr_glyph_length;
+        word_length++;
     }
-  ret[l] = 0;
-  return ret;
-  }
+
+    // Allocate memory for the UTF-32 sequence.
+    UTF32 *utf32_word = (UTF32 *)malloc((word_length + 1) * sizeof(UTF32));
+    UTF32 *utf32_word_ptr = utf32_word;
+    utf8_word_ptr = utf8_word;
+
+    // Convert UTF-8 to UTF-32 sequence.
+    while (*utf8_word_ptr) {
+        int curr_glyph_length = next_utf8_glyph_length(utf8_word_ptr);
+        if (curr_glyph_length == 1) {
+            *utf32_word_ptr = *utf8_word_ptr;
+        }
+        else if (curr_glyph_length == 2) {
+            *utf32_word_ptr = ((*utf8_word_ptr & 0x1F) << 6);
+            *utf32_word_ptr |= (*(utf8_word_ptr + 1) & 0x3F);
+        }
+        else if (curr_glyph_length == 3) {
+            *utf32_word_ptr = ((*utf8_word_ptr & 0x0F) << 12);
+            *utf32_word_ptr |= ((*(utf8_word_ptr + 1) & 0x3F) << 6);
+            *utf32_word_ptr |= (*(utf8_word_ptr + 2) & 0x3F);
+        }
+        else if (curr_glyph_length == 4) {
+            *utf32_word_ptr = ((*utf8_word_ptr & 0x07) << 18);
+            *utf32_word_ptr |= ((*(utf8_word_ptr + 1) & 0x3F) << 12);
+            *utf32_word_ptr |= ((*(utf8_word_ptr + 2) & 0x3F) << 6);
+            *utf32_word_ptr |= (*(utf8_word_ptr + 3) & 0x3F);
+        }
+        utf32_word_ptr++;
+
+        // Prepare for processing the next glyph.
+        utf8_word_ptr += curr_glyph_length;
+    }
+
+    // Null-terminate the UTF-32 sequence.
+    *utf32_word_ptr = 0;
+
+    return utf32_word;
+}
 
 /*===========================================================================
 

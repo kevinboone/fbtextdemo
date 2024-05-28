437114f0cb1181a99cae8632566e8a59af2aa30a
Agustín González <agonzalez@msa.com.ar>
2024-05-28 18:18:28 -0300

    Update test.sh to include centering text examples

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-./fbtextdemo font.ttf -x 30 -y 30 -f 50 -w 700 -c To be or not to be...
-./fbtextdemo font.ttf -x 100 -y 70 -f 30    That is the question.
-./fbtextdemo font.ttf -x 100 -y 100 -f 30    á é í ó ú ñ.
+./fbtextdemo font.ttf -y 30 -f 50 -w 640 -a center -c  To be or not to be...
+./fbtextdemo font.ttf -y 70 -f 30 -w 640 -a center     That is the question
+./fbtextdemo font.ttf -x 30 -y 130 -w 640 -f 30        - This sample works well on a 640x480 framebuffer.
+./fbtextdemo font.ttf -x 30 -y 160 -w 640 -f 30        - Characters such as á, é, í, ó, ú, and ñ are also supported!
